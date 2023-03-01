52f06d3a8dd35168b5a7f1089246ac720b9c3280
Veronica Johnson <cannon2122@yahoo.com>
2023-02-28 23:13:09 -0500

    Pushin up fpr Lesson 1-7

--- src/App.js
+++ src/App.js
@@ -1,39 +1,48 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AddTodoForm from './AddTodoForm';
 import TodoList from './TodoList';
 
 
-
-//Not Easy 
-// Above the App functional component, create a new function named useSemiPersistentState which will be a custom hook
-
-const useSemiPersistentState = () => {
-	const [todoList, setTodoList] = React.useState(
-		
-        //Update the default state for todoList to read your "savedTodoList" item from localStorage
-
-    //  o	Hint: JSON.parse method  Update your default state to parse the value of the "savedTodoList" item
-        
-        
-        JSON.parse(localStorage.getItem('savedTodoList'))
-	);
-//   •	  Open /src/App.js
-// •	  Define a useEffect React hook with todoList as a dependency
-// •	  Inside the side-effect handler function, save the todoList inside localStorage with the key "savedTodoList"
-	React.useEffect(() => {
-
-        //     Update your side-effect function to convert todoList to a string before saving in localStorage
-// o	Hint: JSON.stringify method
-		localStorage.setItem('savedTodoList', JSON.stringify(todoList));
-	}, [todoList]);
-
-	return [todoList, setTodoList];
-};
-
+//oh no Delete the useSemiPersistentState function call from App
 function App() {
-	const [todoList, setTodoList] = useSemiPersistentState();
+	const [todoList, setTodoList] = React.useState([]);
+	const [isLoading, setIsLoading] = React.useState(true);
+//Inside the side-effect handler function, define a new Promise 
+//and pass in a callback function with parameters resolve and reject
+	useEffect(() => {
+	new Promise((resolve) =>
 
-      // Add a return statement in useSemiPersistentState that returns the todoList state variable and setter in an Array 
+	//setTimeout method) with a delay time: 2000 milliseconds (2 seconds)
+
+
+	
+	setTimeout(
+			() =>
+
+			//Then Inside the timeout callback function, call the parameter resolve 
+	//and pass it an Object with property data as a nested empty Object
+	resolve({
+			data: {
+		todoList: JSON.parse(localStorage.getItem('savedTodoList')),
+				},
+				}),	2000)
+// Chain a then method to your Promise constructor and pass it a function with parameter result
+
+
+//Then add another line to set isLoading state to false
+		).then((results) => {
+			setTodoList([...results.data.todoList]);
+			setIsLoading(false);
+		});
+	}, []);
+
+	useEffect(() => {
+
+		//add an if statement to check that isLoading is false before setting localStorage
+		if (!isLoading) {
+			localStorage.setItem('savedTodoList', JSON.stringify(todoList));
+		}
+	}, [todoList, isLoading]);
 
 	const addTodo = (newTodo) => {
 		setTodoList([...todoList, newTodo]);
@@ -48,7 +57,20 @@ function App() {
 		<div>
 			<h1>ToDo List</h1>
 			<AddTodoForm onAddTodo={addTodo} />
-			<TodoList todoList={todoList} onRemoveTodo={removeTodo} />
+			{/* { isLoading  */} 
+
+			
+			{/* Then create a new paragraph element above TodoList with text "Loading..." */}
+			
+			{/* Using a ternary operator inside JSX, if isLoading is true render the loading message, otherwise render the TodoList component */}
+			
+			{/* For the Ternary - This code should display the "Loading" message when isLoading is true and the TodoList 
+			component when isLoading is false, as expected */}
+			{isLoading ? (
+				<p>Loading </p>
+			) : (
+				<TodoList todoList={todoList} onRemoveTodo={removeTodo} />
+			)}
 		</div>
 	);
 }
