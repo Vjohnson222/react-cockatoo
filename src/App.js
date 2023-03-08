2dffd4275f7674e7e6cf14927080139e1e77e481
Veronica Johnson <cannon2122@yahoo.com>
2023-03-07 23:54:40 -0500

    Unable to obtain the Airtable API key. It states email not verified???

--- src/App.js
+++ src/App.js
@@ -2,47 +2,47 @@ import React, { useEffect } from 'react';
 import AddTodoForm from './AddTodoForm';
 import TodoList from './TodoList';
 
-
-//oh no Delete the useSemiPersistentState function call from App
 function App() {
 	const [todoList, setTodoList] = React.useState([]);
 	const [isLoading, setIsLoading] = React.useState(true);
-//Inside the side-effect handler function, define a new Promise 
-//and pass in a callback function with parameters resolve and reject
+
 	useEffect(() => {
-	new Promise((resolve) =>
-
-	//setTimeout method) with a delay time: 2000 milliseconds (2 seconds)
-
-
-	
-	setTimeout(
-			() =>
-
-			//Then Inside the timeout callback function, call the parameter resolve 
-	//and pass it an Object with property data as a nested empty Object
-	resolve({
-			data: {
-		todoList: JSON.parse(localStorage.getItem('savedTodoList')),
-				},
-				}),	2000)
-// Chain a then method to your Promise constructor and pass it a function with parameter result
-
-
-//Then add another line to set isLoading state to false
+		new Promise((resolve) =>
+			setTimeout(
+				() =>
+					resolve({
+						data: {
+							todoList: JSON.parse(localStorage.getItem('savedTodoList')),
+						},
+					}),
+				2000
+			)
 		).then((results) => {
 			setTodoList([...results.data.todoList]);
 			setIsLoading(false);
 		});
-	}, []);
 
-	useEffect(() => {
-
-		//add an if statement to check that isLoading is false before setting localStorage
+		// add an if statement to check that isLoading is false before setting localStorage
 		if (!isLoading) {
 			localStorage.setItem('savedTodoList', JSON.stringify(todoList));
 		}
-	}, [todoList, isLoading]);
+		
+		// Lesson 1-8 Inside the first useEffect hook, replace the placeholder Promise with the following...
+		fetch(
+			`https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/Default?view=Grid%20view&sort[0][field]=Title&sort[0][direction]=asc`,
+			{
+			  method: "GET",
+			  headers: {
+				Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`,
+			  },
+			}
+		  )
+			.then((response) => response.json())
+			.then((result) => {
+			  setTodoList([...result.records]);
+			  setIsLoading(false);
+			});
+	}, []);
 
 	const addTodo = (newTodo) => {
 		setTodoList([...todoList, newTodo]);
@@ -57,15 +57,6 @@ function App() {
 		<div>
 			<h1>ToDo List</h1>
 			<AddTodoForm onAddTodo={addTodo} />
-			{/* { isLoading  */} 
-
-			
-			{/* Then create a new paragraph element above TodoList with text "Loading..." */}
-			
-			{/* Using a ternary operator inside JSX, if isLoading is true render the loading message, otherwise render the TodoList component */}
-			
-			{/* For the Ternary - This code should display the "Loading" message when isLoading is true and the TodoList 
-			component when isLoading is false, as expected */}
 			{isLoading ? (
 				<p>Loading </p>
 			) : (
--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -4,7 +4,8 @@ import React from 'react';
 const TodoListItem = ({ todo, onRemoveTodo }) => {
 	return (
 		<div>
-			<li> {todo.title} </li>
+			{/* Update the todo item title to reference the new object format (hint: todo.fields.Title) */}
+			<li> {todo.fields.title} </li>
 			{/* Add a button element, type "button", inside the list item with text "Remove"  */}
 			<button type='button' onClick={() => onRemoveTodo(todo.id)}>
 					Remove
